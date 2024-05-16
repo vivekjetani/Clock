91c854c5e8e2b41b09b19a669a260da4ce4cbc17
Vivek jetani <jetanivivek321@gmail.com>
2024-05-16 09:05:49 +0530

    create a base gui for clock

---
+++ clock.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+root.geometry("1920x1080")
+root.config(bg="Black")
+
+
+
+mainloop()
