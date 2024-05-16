27b9803b90d868b884c9b25a26ed9a504a53f390
Vivek jetani <jetanivivek321@gmail.com>
2024-05-16 09:12:29 +0530

    add working time function

--- clock.py
+++ clock.py
@@ -7,6 +7,14 @@ root.title("Clock")
 root.geometry("1920x1080")
 root.config(bg="Black")
 
+def time():
+    t = strftime("%H:%M:%S %p")
+    label.config(text=t)
 
 
+label = Label(root ,font=("Helvetica",180), background="Black",foreground="White")
+label.pack(anchor='s')
+
+
+time()
 mainloop()
