08dc80e775f6da5b364a4c3231e9333d0f9e568a
Vivek jetani <jetanivivek321@gmail.com>
2024-05-16 09:22:10 +0530

    add date function and aline it

--- clock.py
+++ clock.py
@@ -10,13 +10,22 @@ root.config(bg="Black")
 def time():
     t = strftime("%H:%M:%S %p")
     label.config(text=t)
-    label.place(relx=0.5,rely=0.5,anchor="center")
+    label.place(relx=0.5,rely=0.45,anchor="center")
     label.after(1000,time)
+
+def date():
+    d = strftime("%d %b %y")
+    label2.config(text=d)
+    label2.place(relx=0.5,rely=0.65,anchor="center")
+    label2.after(1000,date)
 
 
 label = Label(root ,font=("Helvetica",180), background="Black",foreground="White")
 label.pack(anchor='s')
 
+label2 =Label(root,font=("Helvetica",90), background="Black",foreground="White")
+label2.pack(anchor='s')
 
 time()
+date()
 mainloop()
